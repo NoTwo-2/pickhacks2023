72736d71e7394b8ce6b870e2b74e6d09b4590d34
Hari Patel <haripatel@Haris-MacBook-Pro.local>
2023-04-15 13:15:29 -0500

    autocomplete implemented

--- frontend/templates/test.js
+++ frontend/templates/test.js
@@ -1,17 +1,23 @@
 var map, originMarker, destMarker,originAutocomplete, destAutocomplete;;
 
-function initMap() {
+async function initMap() {
   // Create a map centered on the United States
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 39.8283, lng: -98.5795},
     zoom: 4
   });
+  const [{ Map }] = await Promise.all([google.maps.importLibrary("places")]);
+  
+  // Create the input HTML element, and append it
+  const input = document.createElement("input");
+  const pac = new google.maps.places.PlaceAutocompleteElement({
+    inputElement: input,
+    map: map,
+  });
 
-originAutocomplete = new google.maps.places.Autocomplete(
-    document.getElementById('origin'), {types: ['geocode']});
-  destAutocomplete = new google.maps.places.Autocomplete(
-    document.getElementById('destination'), {types: ['geocode']});
+  document.body.appendChild(pac.element);
 }
+
 
 function calculateDistance() {
   var origin = document.getElementById('origin').value;
@@ -30,6 +36,32 @@ function calculateDistance() {
       console.log(distance);
       var consoleDiv = document.getElementById("distance");
       consoleDiv.innerHTML += distance;
+
+      // Create orgin and Destination markers for search term
+      var geocoder = new google.maps.Geocoder();
+      geocoder.geocode({ 'address': origin }, function(results, status) {
+        if (status === 'OK') {
+          originMarker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location,
+            title: origin
+          });
+        } else {
+          alert('Error: ' + status);
+        }
+      });
+
+      geocoder.geocode({ 'address': destination }, function(results, status) {
+        if (status === 'OK') {
+          destMarker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location,
+            title: destination
+          });
+        } else {
+          alert('Error: ' + status);
+        }
+      });
 
     } else {
       alert('Error: ' + status);
